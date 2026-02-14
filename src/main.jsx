f48388a436ac97f0eceed88fc046088b7a1f6994
Assadmessi <ayehtetheinmessi@gmail.com>
2026-02-15 02:43:23 +0300

    unhead-5

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { createHead, UnheadProvider } from "@unhead/react/client";
+import { ThemeProvider } from "./context/ThemeContext"; // <-- adjust path if needed
 import App from "./App";
 import "./index.css";
-
-import { createHead, UnheadProvider } from "@unhead/react/client";
 
 const head = createHead();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UnheadProvider head={head}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </UnheadProvider>
   </React.StrictMode>
 );
