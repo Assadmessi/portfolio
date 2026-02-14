0ef8f708df4bf95df95de2a305fae6cf5ec75f4f
Assadmessi <ayehtetheinmessi@gmail.com>
2026-02-15 02:27:33 +0300

    unhead

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { createHead, HeadProvider } from "@unhead/react/client";
+import { createHead, UnheadProvider } from "@unhead/react/client";
 
 const head = createHead();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HeadProvider head={head}>
+    <UnheadProvider head={head}>
       <App />
-    </HeadProvider>
+    </UnheadProvider>
   </React.StrictMode>
 );
